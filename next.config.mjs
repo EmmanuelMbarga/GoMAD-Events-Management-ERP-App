70349db3401a0ba595b8f41f4f4a90cffc36fe2c
MadickAngeCesar <madick.angecesar@ictuniversity.edu.cm>
2025-01-25 06:59:28 +0100

    refactor: simplify Next.js configuration by removing security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,9 @@
 import withPWA from "next-pwa";
 
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; object-src 'none';",
-  },
-];
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
+// Export the configuration with PWA support
 export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+  ...nextConfig,
+});
