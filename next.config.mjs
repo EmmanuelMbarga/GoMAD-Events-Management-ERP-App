e03bbe74f493efa373e3f542007cb2181e470ae2
MadickAngeCesar <madick.angecesar@ictuniversity.edu.cm>
2025-01-25 06:51:45 +0100

    refactor: enhance Next.js PWA configuration and implement security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,26 @@
 import withPWA from "next-pwa";
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; object-src 'none';",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
   async headers() {
     return [
       {
         source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; object-src 'none';",
-          },
-        ],
+        headers: securityHeaders,
       },
     ];
   },
 };
 
-export default withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
